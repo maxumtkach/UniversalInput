d55ee7c77729fce83dcd59c997921fd49347b256
maxumtkach <makstkach@bigmir.net>
2019-05-20 12:00:33 +0300

    //1.2.1 universal input form

---
+++ app/src/main/java/com/example/universalinput/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.universalinput;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TextView messageText;
+    private EditText nameEditText;
+    private EditText emailEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        messageText = findViewById(R.id.text_message);
+        nameEditText = findViewById(R.id.editText_name);
+        emailEditText = findViewById(R.id.editText_email);
+
+        Button clickOkButton = findViewById(R.id.button_ok);
+        clickOkButton.setOnClickListener(clickOkListener);
+
+        Button clickClearButton = findViewById(R.id.button_clear);
+        clickClearButton.setOnClickListener(clickClearListener);
+
+    }
+
+    private final View.OnClickListener clickOkListener = new View.OnClickListener() {
+        @SuppressLint("SetTextI18n")
+        @Override
+        public void onClick(View v) {
+
+            messageText.setText("Подписка на рассылку успешно оформлена для пользователя " + nameEditText.getText().toString() + " на электронный адрес " + emailEditText.getText().toString());
+        }
+    };
+
+    private final View.OnClickListener clickClearListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            messageText.setText("");
+        }
+    };
+}
